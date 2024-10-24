0e03e9fde7c7e801c14591cef6c70660eb8583d2
Rajabali07 <rajabalijurayevich@gmail.com>
2024-10-24 10:25:38 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,75 @@
+"use strict" // qatiy rejim o'zgaruvchilar uchun
+
+// // Data Typelar haqida
+
+// // Number 
+// let number = 5.6 // data type number  numberlar 
+// console.log(8/0) // infinitiy yani cheksizlik
+// console.log("aka" * 5) // NaN not a number  mumkin emas sonni stringa ko'paytirish, qo'shish
+
+// // string
+// const ClientName = "Mirjalol";
+
+// //Boolean 
+// const isMarried = True;
+
+
+// //Null bu yo'q malumot so'rasak
+// console.log(Mirjalol)
+
+// // Undefined bu o'zgaruvchi bor lekin qiymati yo'q holda chiqadi
+// let und; 
+// console.log(und)
+
+// // Object bu kalit qiymat ko'rinishida bo'ladi malumot
+// const thief = {
+//     age:60,
+//     jacket:"black",
+//     isLong: false,
+// }
+// // qiymatlarni olish uchun
+// console.log(thief.age);
+
+
+// Array
+
+// const colors = ["red", "black", 12, [], {}]
+// console.log(colors[0]); //index bo'yicha olinadi
+
+// // Foydalanuvchi bilan muloqot
+// const place = confirm("Are you here");
+// console.log(place);
+
+// const currentPlace =prompt("Qayerdasan Mirjalol?") // promt orqali foydalanuvchiga savol berib javob olishimiz mumkin  input vazifasini bajaradi
+
+// const favouriteColor = [];
+// favouriteColor[0] = "Smart" // massivga malumot qo'shish
+// console.log(favouriteColor)
+
+
+// // Interpalatsiya 
+
+// const channel = prompt("What is your favourite youtuber");
+// console.log(`https://youtube.com/${channel}`);  // pythondagi f stringa o'xhsaydi
+
+
+
+// Operatorlar
+
+// console.log(9 * "9");
+// incr, decr
+
+// let incr = 5;
+// let decr = 5;
+
+// incr++; // sonni birga oshiradi
+// decr--; // sonni birga kamaytiradi
+
+// console.log(incr)
+// console.log(decr)
+
+// == bu qiymati bo'yicha tenglab ko'radi
+// === bu qiymati va data type bo'yicha
+
+// && = va  || = yoki  ! not ni bildiradi shart operatorlari
+
